60a22daa20d3166ae631c00667e329c3c715d101
dhcarleton-glitch <dhagens.carleton@gmail.com>
2026-03-27 16:46:34 -0400

    datacleaning.r
    This is the code used to clean the data, and perform a list-wise deletion so that all attentional control task scores with missing scores were removed from the dataset.

--- datacleaning.r
+++ datacleaning.r
@@ -1,15 +1,20 @@
+#### Load Packages
 library(readxl)
 library(dplyr)
 library(janitor)
 library(writexl)
 
+#### Import Data
 ac_data <- read_excel("AttentionControl_Dataset.xlsx")
 names(ac_data_f)
 
+#### Remove demographic information to isolate the attentional control tasks
 ac_data_f <- ac_data[-c(5,6,7,8,9,10,11,12,13,14)]
 
+#### Make a list to incluce only the attentional control tasks 
 ac_data_s <- ac_data[c(1,2,3,4)]
 
+#### Find those under each attentional control task, to find those which have no scores
 which(is.na(ac_data_f[c("Squared_Flanker.Points", "Squared_Flanker.Points", "Squared_Simon.Points")]))
 sum(is.na(ac_data_f[c("Squared_Flanker.Points", "Squared_Flanker.Points", "Squared_Simon.Points")]))
 is.na(ac_data_f[c("Squared_Flanker.Points")])
@@ -34,6 +39,7 @@ ac_data_omit_full <- ac_data_f %>%
 ac_data_omit_tria <- ac_data %>% filter(!is.na(Squared_Stroop.points))
 
 
+#### Write to a new excel sheet those with empty values removed
 write_xlsx(ac_data_omit, "ac_data_omit.xlsx")
 write_xlsx(ac_data_omit_full, "ac_data_omit_full.xlsx")
 
